91c7ec98cdd32a624a4b902df04366b7d67ddf7c
Ed Grosvenor <ed@artisan.build>
2026-08-13 14:51:23 +0200

    Converge Built for Cloud authority surface

--- config/auth.php
+++ config/auth.php
@@ -2,13 +2,23 @@
 
 return [
     'defaults' => [
-        'guard' => null,
+        'guard' => 'web',
         'passwords' => null,
     ],
 
-    'guards' => [],
+    'guards' => [
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+    ],
 
-    'providers' => [],
+    'providers' => [
+        'users' => [
+            'driver' => 'database',
+            'table' => 'users',
+        ],
+    ],
 
     'passwords' => [],
     'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
