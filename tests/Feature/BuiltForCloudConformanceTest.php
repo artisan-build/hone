2b2cb878a1d4051f05ead4ee9f1b44f80a35a837
Ed Grosvenor <ed@artisan.build>
2026-09-26 14:25:54 +0200

    Align Hone with the v0.18 contract

--- tests/Feature/BuiltForCloudConformanceTest.php
+++ tests/Feature/BuiltForCloudConformanceTest.php
@@ -136,6 +136,7 @@ it('passes the package consumer conformance spec for the complete Hone consumer'
             CredentialMintCommand::class,
             CredentialRevokeCommand::class,
             CredentialRotateCommand::class,
+            'ArtisanBuild\BuiltForCloud\Commands\FreshCommand',
             HmacRewrapCommand::class,
             InstallOperatorCredentialCommand::class,
             OutboxDrainCommand::class,
@@ -164,7 +165,6 @@ it('passes the package consumer conformance spec for the complete Hone consumer'
             'ArtisanBuild\BuiltForCloud\Http\Controllers\UiHome|built-for-cloud.ui.personal_credentials|1',
             'ArtisanBuild\BuiltForCloud\Http\Controllers\UiHome|built-for-cloud.ui.session_management|1',
             'ArtisanBuild\BuiltForCloud\LandingManifest|built-for-cloud.manifest|1',
-            'ArtisanBuild\BuiltForCloud\LandingPageRegistrar|built-for-cloud.ui.landing_page|1',
             'ArtisanBuild\BuiltForCloud\UiCredentialPurposes|built-for-cloud.ui.credential_purposes|1',
         ]),
         'mcp_delegated' => $sorted([
